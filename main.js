c7bd1d99d57511529f892070eb37f62f43eb7cd2
Emrecan Şuşter <emrecansuster@outlook.com>
2021-05-10 22:03:35 +0300

    finalize the project

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 function appendTheNum(e) {
     const num = e.explicitOriginalTarget.innerText; 
+    alterShownValue(num); 
+}
+
+function alterShownValue(num) {
     let shownValue = document.querySelector("#shownValue");
     if (num == "C") {
         shownValue.value = 0;
-    } else if (num == "") {
+        memorial = {...memorialTemplate};
+    } else if (num == "" || num == ".") {
         if (!shownValue.value.includes(".")) {
             shownValue.value = shownValue.value + ".";
         }
-    } else if (shownValue.value == 0) {
+    } else if (shownValue.value === "0") {
         shownValue.value = num;
     } else {
         shownValue.value = shownValue.value + num;
@@ -24,6 +29,7 @@ function prepareNumbers(container) {
         button.classList.add("number");
         button.classList.add("button");
         button.textContent = num;
+        button.id = `button${num}`
         if (num == ".") {
             button.innerHTML = `<i class="fas fa-circle"></i>`;
             button.style["font-size"] = "3vh";
@@ -37,21 +43,48 @@ const container = document.querySelector("#numbers");
 prepareNumbers(container);
 
 function add(a,b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a,b) {
-    return a-b;
+    return +a - +b;
 }
 
 function multiply(a,b) {
-    return a*b;
+    return +a * +b;
 }
 
 function divide(a,b) {
-    return a/b;
+    return +a / +b;
 }
 
-function operate(operation, a, b) {
-    return operation(a,b);
+let memorial = {
+    value: 0,
+    operation: 0
+};
+
+const memorialTemplate = { ...memorial };
+
+function operate(operation) {
+    let shownValue = document.querySelector("#shownValue");
+    if (memorial.value == 0 & memorial.operation == 0) {
+        memorial.value = shownValue.value;
+        memorial.operation = operation;
+        shownValue.value = 0;
+    } else {
+        if (operation) {
+            shownValue.value = operation(memorial.value, shownValue.value);
+        } else if (memorial.operation != 0) {
+            shownValue.value = memorial.operation(memorial.value, shownValue.value);
+        }
+        memorial = {...memorialTemplate};
+    }
 }
+
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+    const keyCode = e.code;
+    if (!isNaN(key) || keyCode == "KeyC" || key == ".") {
+        alterShownValue(key);
+    }
+})
